4821e00c6f7d55efae97797f52cedaefbdbf959e
John J. Reiland <jreiland1@pride.hofstra.edu>
2023-12-20 17:28:40 -0500

    Test different motor number

--- Robot.c
+++ Robot.c
@@ -3,30 +3,30 @@
 extern void robotInit(){
     initHat();
     initMotors();
-    run(RELEASE, 1);    //left motor off
-    run(RELEASE, 2);    //right motor off
+    run(RELEASE, 0);    //left motor off
+    run(RELEASE, 1);    //right motor off
 }
 
 extern void setLeftSpeed(int speed){
     //add error checking later
-    setSpeed(motors[1].pwm, speed);
+    setSpeed(motors[0].pwm, speed);
 }
 
 extern void setRightSpeed(int speed){
     //add error checking later
-    setSpeed(motors[2].pwm, speed);
+    setSpeed(motors[1].pwm, speed);
 }
 
 extern void stop(){
-    run(RELEASE, 1);    //left motor off
-    run(RELEASE, 2);    //right motor off
+    run(RELEASE, 0);    //left motor off
+    run(RELEASE, 1);    //right motor off
 }
 
 extern void forward(int speed, int seconds){
     setLeftSpeed(speed);
     setRightSpeed(speed);
-    run(FORWARD, 1);    //left motor forward
-    run(FORWARD, 2);    //right motor forward
+    run(FORWARD, 0);    //left motor forward
+    run(FORWARD, 1);    //right motor forward
     if (seconds > 0){   //definite
         sleep(seconds);
         stop();
@@ -36,8 +36,8 @@ extern void forward(int speed, int seconds){
 extern void backward(int speed, int seconds){
     setLeftSpeed(speed);
     setRightSpeed(speed);
-    run(BACKWARD, 1);    //left motor backward
-    run(BACKWARD, 2);    //right motor backward
+    run(BACKWARD, 0);    //left motor backward
+    run(BACKWARD, 1);    //right motor backward
     if (seconds > 0){   //definite
         sleep(seconds);
         stop();
@@ -47,8 +47,8 @@ extern void backward(int speed, int seconds){
 extern void right(int speed, int seconds){
     setLeftSpeed(speed);
     setRightSpeed(speed);
-    run(FORWARD, 1);    //left motor forward
-    run(BACKWARD, 2);    //right motor backward
+    run(FORWARD, 0);    //left motor forward
+    run(BACKWARD, 1);    //right motor backward
     if (seconds > 0){   //definite
         sleep(seconds);
         stop();
@@ -58,8 +58,8 @@ extern void right(int speed, int seconds){
 extern void left(int speed, int seconds){
     setLeftSpeed(speed);
     setRightSpeed(speed);
-    run(BACKWARD, 1);    //left motor backward
-    run(FORWARD, 2);    //right motor forward
+    run(BACKWARD, 0);    //left motor backward
+    run(FORWARD, 1);    //right motor forward
     if (seconds > 0){   //definite
         sleep(seconds);
         stop();
