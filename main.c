1af39c3233424b35591c91dbf186bb9fda0b78fd
John J. Reiland <jreiland1@pride.hofstra.edu>
2023-12-20 21:09:28 -0500

    was a hardware problem

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 
 int main(int argc, char *argv[]){
     robotInit();
-    //forward(50, 10);
-    //left(10, 2);
+    forward(50, 10);
+    left(10, 2);
     backward(60, 1);
-    //right(10, 5);
-    //forward(100, 3);
+    right(10, 5);
+    forward(100, 3);
     stop();
 }
