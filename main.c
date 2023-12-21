65c8c2989a38163e2b508b03655cf9b69874f035
John J. Reiland <jreiland1@pride.hofstra.edu>
2023-12-21 00:19:00 -0500

    Added sensor code

--- main.c
+++ main.c
@@ -5,20 +5,90 @@
 int main(int argc, char *argv[]){
     robotInit();
 
-    printf("execute forward\n");
-    forward(100, 2);
+    // printf("execute forward\n");
+    // forward(100, 2);
 
-    printf("execute left\n");
-    left(100, 2);
+    // printf("execute left\n");
+    // left(100, 2);
 
-    printf("execute backward\n");
-    backward(100, 2);
+    // printf("execute backward\n");
+    // backward(100, 2);
 
-    printf("execute right\n");
-    right(100, 2);
+    // printf("execute right\n");
+    // right(100, 2);
 
-    printf("execute forward2\n");
-    forward(100, 2);
+    // printf("execute forward2\n");
+    // forward(100, 2);
+
+    double pulse_start, pulse_end, pulse_duration;
+    double distance;
+
+
+    bcm2835_gpio_fsel(ECHO, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_fsel(TRIG, BCM2835_GPIO_FSEL_OUTP);
+
+    bcm2835_gpio_write(TRIG,LOW);
+
+    printf("Waiting for sensor to settle\n");
+
+    bcm2835_delay(2000);
+
+    while (1){
+        
+
+        printf("Set TRIG to true\n");
+
+        bcm2835_gpio_write(TRIG,HIGH);
+
+        printf("Sleep\n");
+
+        bcm2835_delayMicroseconds(10);
+
+        printf("Set TRIG to false\n");
+
+        bcm2835_gpio_write(TRIG,LOW);
+
+        printf("First while loop\n");
+
+        while (bcm2835_gpio_lev(ECHO)==0)
+            pulse_start = get_time();//get the time in microseconds
+
+        printf("pulse start: %Lf\n",pulse_start);
+
+        printf("Second while loop\n");
+
+        while(bcm2835_gpio_lev(ECHO)==1)
+            pulse_end = get_time();//get the time in micorseconds
+
+        printf("pulse end:%Lf\n",pulse_end);
+
+        //pulse_duration = (double)(pulse_end - pulse_start)/CLOCKS_PER_SEC;
+        //CLOCKS_PER_SEC is equal to 1000000 on the raspberry pi.
+        //in order for this to work, you have to get the time in micro seconds.
+
+        pulse_duration = (pulse_end - pulse_start)/1000000;
+
+        printf("pulse duration:%Lf\n",pulse_duration);
+
+        distance = pulse_duration * 17150;
+
+        printf("Distance:%.2f cm\n",distance);
+
+        forward(100, -1);
+
+        if (distance < 5 || distance > 1000){
+            stop();
+            backward(50, 1);
+            stop();
+            right(2, 1);
+            stop();
+            continue;
+        }
+    }
+
+    bcm2835_close();
 
     stop();
+
+    return 0;
 }
--- sensor.c
+++ sensor.c
@@ -26,65 +26,65 @@ extern void gpio_reset()
 	bcm2835_gpio_fsel(RPI_V2_GPIO_P1_16, BCM2835_GPIO_FSEL_INPT);
 }
 
-int main(int argc, char **argv)
-{
+// int main(int argc, char **argv)
+// {
 	
-	double pulse_start, pulse_end, pulse_duration;
-	double distance;
+// 	double pulse_start, pulse_end, pulse_duration;
+// 	double distance;
 
-	if (!bcm2835_init())
-		return 1;
+// 	if (!bcm2835_init())
+// 		return 1;
 
-	bcm2835_gpio_fsel(ECHO, BCM2835_GPIO_FSEL_INPT);
-	bcm2835_gpio_fsel(TRIG, BCM2835_GPIO_FSEL_OUTP);
+// 	bcm2835_gpio_fsel(ECHO, BCM2835_GPIO_FSEL_INPT);
+// 	bcm2835_gpio_fsel(TRIG, BCM2835_GPIO_FSEL_OUTP);
 
-	bcm2835_gpio_write(TRIG,LOW);
+// 	bcm2835_gpio_write(TRIG,LOW);
 
-	printf("Waiting for sensor to settle\n");
+// 	printf("Waiting for sensor to settle\n");
 
-	bcm2835_delay(2000);
+// 	bcm2835_delay(2000);
 
-	printf("Set TRIG to true\n");
+// 	printf("Set TRIG to true\n");
 
-	bcm2835_gpio_write(TRIG,HIGH);
+// 	bcm2835_gpio_write(TRIG,HIGH);
 
-	printf("Sleep\n");
+// 	printf("Sleep\n");
 
-	bcm2835_delayMicroseconds(10);
+// 	bcm2835_delayMicroseconds(10);
 
-	printf("Set TRIG to false\n");
+// 	printf("Set TRIG to false\n");
 
-	bcm2835_gpio_write(TRIG,LOW);
+// 	bcm2835_gpio_write(TRIG,LOW);
 
-	printf("First while loop\n");
+// 	printf("First while loop\n");
 
-	while (bcm2835_gpio_lev(ECHO)==0)
-		pulse_start = get_time();//get the time in microseconds
+// 	while (bcm2835_gpio_lev(ECHO)==0)
+// 		pulse_start = get_time();//get the time in microseconds
 
-	printf("pulse start: %Lf\n",pulse_start);
+// 	printf("pulse start: %Lf\n",pulse_start);
 
-	printf("Second while loop\n");
+// 	printf("Second while loop\n");
 
-	while(bcm2835_gpio_lev(ECHO)==1)
-		pulse_end = get_time();//get the time in micorseconds
+// 	while(bcm2835_gpio_lev(ECHO)==1)
+// 		pulse_end = get_time();//get the time in micorseconds
 
-	printf("pulse end:%Lf\n",pulse_end);
+// 	printf("pulse end:%Lf\n",pulse_end);
 
-	//pulse_duration = (double)(pulse_end - pulse_start)/CLOCKS_PER_SEC;
-	//CLOCKS_PER_SEC is equal to 1000000 on the raspberry pi.
-	//in order for this to work, you have to get the time in micro seconds.
+// 	//pulse_duration = (double)(pulse_end - pulse_start)/CLOCKS_PER_SEC;
+// 	//CLOCKS_PER_SEC is equal to 1000000 on the raspberry pi.
+// 	//in order for this to work, you have to get the time in micro seconds.
 
-	pulse_duration = (pulse_end - pulse_start)/1000000;
+// 	pulse_duration = (pulse_end - pulse_start)/1000000;
 
-	printf("pulse duration:%Lf\n",pulse_duration);
+// 	printf("pulse duration:%Lf\n",pulse_duration);
 
-	distance = pulse_duration * 17150;
+// 	distance = pulse_duration * 17150;
 
-	printf("Distance:%.2f cm\n",distance);
+// 	printf("Distance:%.2f cm\n",distance);
 
-	gpio_reset();
+// 	gpio_reset();
 
-	bcm2835_close();
-	return 0;
+// 	bcm2835_close();
+// 	return 0;
 
-}
+// }
