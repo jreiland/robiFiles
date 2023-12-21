fed47deec6fdda00bba1796487f33f4e2184b7ca
John J. Reiland <jreiland1@pride.hofstra.edu>
2023-12-20 21:41:20 -0500

    Change speed

--- main.c
+++ main.c
@@ -6,19 +6,19 @@ int main(int argc, char *argv[]){
     robotInit();
 
     printf("execute forward\n");
-    forward(100, 10);
+    forward(100, 2);
 
     printf("execute left\n");
-    left(100, 10);
+    left(100, 2);
 
     printf("execute backward\n");
-    backward(100, 10);
+    backward(100, 2);
 
     printf("execute right\n");
-    right(100, 10);
+    right(100, 2);
 
     printf("execute forward2\n");
-    forward(100, 10);
+    forward(100, 2);
 
     stop();
 }
