c7ea87c6feb6b5bc86b91d4659e82019616d3d83
John J. Reiland <jreiland1@pride.hofstra.edu>
2023-12-20 21:14:40 -0500

    add debugging printf statements

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include "Robot.h"
 #include "Sensor.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]){
     robotInit();
+
+    printf("execute forward\n");
     forward(50, 10);
+
+    printf("execute left\n");
     left(10, 2);
+
+    printf("execute backward\n");
     backward(60, 1);
+
+    printf("execute right\n");
     right(10, 5);
+
+    printf("execute forward2\n");
     forward(100, 3);
+
     stop();
 }
