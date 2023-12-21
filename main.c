7fc7cc7f985efe1af548e96b79c4be3687b18a59
John J. Reiland <jreiland1@pride.hofstra.edu>
2023-12-20 20:53:40 -0500

    try another way for testing

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[]){
     robotInit();
-    forward(50, 10);
+    //forward(50, 10);
     //left(10, 2);
-    //backward(60, 1);
+    backward(60, 1);
     //right(10, 5);
     //forward(100, 3);
     stop();
